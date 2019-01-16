13810c540ab87f2c786557421097bcddca722e31
Ali Asaf <aliasafpolat@hotmail.com>
2019-01-16 15:50:59 +0300

    JFrame Here.

---
+++ Fundemantal-Java/CreateJFrame.java
@@ -0,0 +1,13 @@
+package com.asaf;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame pencere =new JFrame();//Pencere tanımı yapıldı.
+        pencere.setSize(640,480); //Boyutlar ayarlandı.
+        pencere.setVisible(true); //Görünür hale getirildi.
+		
+    }
+}
