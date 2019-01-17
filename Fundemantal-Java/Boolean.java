eae53078722375fb01ea4ba44f67b4232909b9ca
Ali Asaf <aliasafpolat@hotmail.com>
2019-01-17 21:07:38 +0300

    BooleanS

---
+++ Fundemantal-Java/Boolean.java
@@ -0,0 +1,21 @@
+package com.asaf;
+
+
+import java.util.Scanner;
+
+import static java.io.File.*;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+
+	boolean x=true;
+	boolean y=false;
+
+	System.out.println(x&y);
+	   System.out.println((!x)+" "+(!y));
+	   System.out.println(x|y);
+	   System.out.println(x^y);
+    }
+}
