145aa204711a8e1c7660dffbe2874b9005545cd4
Ali Asaf <aliasafpolat@hotmail.com>
2019-01-18 22:06:39 +0300

    parse int

---
+++ Fundemantal-Java/StrToInteger.java
@@ -0,0 +1,13 @@
+package com.asaf;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String numberStr="1581";
+
+        System.out.println("This is String :"+numberStr);
+        int number=Integer.parseInt(numberStr);
+        System.out.println("This is integer :"+number);
+
+    }
+}
