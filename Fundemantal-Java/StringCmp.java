27ae19b04687b93b50ef380afd6eefd60646a3bd
Ali Asaf <aliasafpolat@hotmail.com>
2019-01-19 16:45:36 +0300

    Strings Operands.

---
+++ Fundemantal-Java/StringCmp.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Main {
+    public static void main(String args[]){
+        String obj1="Ali Asaf";
+        String obj2="Polat";
+        String obj3=obj1+obj2;
+
+        System.out.println("Length of first str is: "+obj1.length());
+        System.out.println("Char at 3th order is :"+obj2.charAt(3));
+        if (!obj3.equals("Reis"))
+        System.out.println("This words is not same!");
+    }
+}
