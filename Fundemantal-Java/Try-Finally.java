1535405d0bb3c1b5a09abc49a92a510c07030a87
Ali Asaf <aliasafpolat@hotmail.com>
2019-01-23 15:42:06 +0300

    Finally block added.

---
+++ Fundemantal-Java/Try-Finally.java
@@ -0,0 +1,25 @@
+package com.asaf;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try{
+            int a=args.length;
+            System.out.println("a = "+a);
+            int b=42/a;
+            int c[]={5};
+            c[44]=97;
+
+        }catch(ArithmeticException e){
+            System.out.println("Divide by zero "+e);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Array index oob :"+e);
+        }finally {
+            System.out.println("This is Finally block BRO!");
+        }
+
+        System.out.println("After Try-Catch blocks.");
+
+    }
+}
